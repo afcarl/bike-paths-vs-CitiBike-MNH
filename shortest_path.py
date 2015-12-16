e1917bb4a681989caf8773bb6e31819853cc243c
vipassana <vipassana.me@gmail.com>
2015-12-16 18:12:36 -0500

    Yaaaaaaaaaaaaay

--- shortest_path.py
+++ shortest_path.py
@@ -1,9 +1,56 @@
-import json
-import requests
+import geopandas as gpd
 
-response = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=40.7528,-73.9765&destination=40.750779,-73.993519&key=APIKEY&mode=bicycling')
-json_data = json.loads(response.text)
+import googlemaps
+
+import simplekml
+
+import matplotlib.pyplot as plt
+
+import mplleaflet
+
+from fiona.crs import from_epsg
+
+from shapely import geometry as geometry
+
+from polyline.codec import PolylineCodec
+
+import pandas as pd
+
+# KML TO GEOJSON
 
 
-with open('directions.json', 'w') as fp:
-    json.dump(json_data, fp)
+cbike = pd.read_csv('top50.csv')
+
+gmaps = googlemaps.Client(key='')
+
+dirs = []
+
+
+for i in cbike.index[:5]:
+
+    xa = cbike['start station longitude'][i]
+    ya = cbike['start station latitude'][i]
+    xb = cbike['end station longitude'][i]
+    yb = cbike['end station latitude'][i]
+    directions = gmaps.directions((ya,xa), (yb,xb), mode='bicycling')
+
+    #print directions
+    codec = PolylineCodec()
+
+    path = []
+
+    for s in directions[0]['legs'][0]['steps']:
+
+        path += codec.decode(s['polyline']['points'])
+    # swap lat and lon
+    pp = zip(*path)
+    path = zip(pp[1], pp[0])
+    lines = geometry.LineString(path)
+    kml = simplekml.Kml()
+    ls = kml.newlinestring(name='sample')
+    ls.coords = lines.coords
+    kml.save("/data/path" + str(i) + ".kml")
+
+
+
+
